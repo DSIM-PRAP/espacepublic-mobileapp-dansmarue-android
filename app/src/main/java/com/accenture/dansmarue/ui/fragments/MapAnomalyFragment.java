7876140a600a8cdf1b36b5a6d0ff37e4bb5cf08f
Corbel <vincent.corbel@accenture.com>
2026-04-15 09:56:41 +0200

    BV-220 : Passage de la carte en mode plan

--- app/src/main/java/com/accenture/dansmarue/ui/fragments/MapAnomalyFragment.java
+++ app/src/main/java/com/accenture/dansmarue/ui/fragments/MapAnomalyFragment.java
@@ -52,7 +52,7 @@ public class MapAnomalyFragment extends Fragment implements OnMapReadyCallback, 
 
         if (isAdded()) {
 
-            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
 //        Central Paris > Notre Dame: 48.853152, 2.349891
             LatLng centralParis = new LatLng(48.853152, 2.349891);
